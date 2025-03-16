0da126e42ec470c1022d988aa6edc864144220b5
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-03-16 22:47:06 +0200

    update try fetch from  compendium

---
+++ src/crud.js
@@ -0,0 +1,27 @@
+// const postToAdd = {
+//   author: 'Mango',
+//   body: 'CRUD is awesome',
+// };
+
+// const options = {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// const postId = 1;
+
+// fetch('https://jsonplaceholder.typicode.com/posts', options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// Change this number to fetch different post
+const postId = 1;
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then(response => response.json())
+  .then(post => console.log(post))
+  .catch(error => console.log(error));
